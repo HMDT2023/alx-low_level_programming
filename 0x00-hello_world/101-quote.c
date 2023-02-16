123c36669e335e79ee6ac2c8747ad4e524844510
HMDT2023 <merveilhouenagnon@gmail.com>
2023-02-16 13:38:40 -0800

    prints program in c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 57);
+	return (1);
+}
