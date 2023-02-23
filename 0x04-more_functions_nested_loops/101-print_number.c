9c3849045456d1fdce5710ddf53348cfbf8f788b
HMDT2023 <merveilhouenagnon@gmail.com>
2023-02-23 13:19:19 -0800

    function that prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+*print_number - Entry to code
+*
+* @n : integer to print
+*
+* Return: Always all number (Success)
+*/
 void print_number(int n)
 {
 	unsigned int num = n;
