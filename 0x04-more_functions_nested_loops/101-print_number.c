3ce1bb9ef55e96c4b6b84b660dcb9b084c9b0283
HMDT2023 <merveilhouenagnon@gmail.com>
2023-02-23 13:16:12 -0800

    function that prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,82 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-
-/**
-*two_digit - Entry to code
-*
-* @n : variable
-*
-* Return: Always all number (Success)
-*/
-
-void two_digit(int n)
-{
-	int i = 0;
-	int j = 0;
-
-	i = n % 10;
-	j = (n - i);
-	_putchar((j / 10) + '0');
-	_putchar(i  + '0');
-}
-
-
-/**
-*three_digit - Entry to code
-*
-* @n : variable
-*
-* Return: Always all number (Success)
-*/
-
-void three_digit(int n)
-{
-	int i = 0;
-	int j = 0;
-	int k = 0;
-
-	i = n % 10;
-	j = (n - j) % 100;
-	k = (n - i - j);
-	_putchar(k  + '0');
-	_putchar((j / 10) + '0');
-	_putchar(i / 100 + '0');
-}
-
-
-/**
-*four_digit - Entry to code
-*
-* @n : first integer
-*
-* Return: Always all number (Success)
-*/
-void four_digit(int n)
-{
-	int i = 0;
-	int t = 0;
-	int j = 0;
-	int k = 0;
-
-	j = n % 10;
-	t = (n - j) % 100;
-	k = (n - j - t) % 1000;
-	i = (n - t - j - k);
-	_putchar((i / 1000) + '0');
-	_putchar((k / 100) + '0');
-	_putchar((t / 10) + '0');
-	_putchar(j + '0');
-}
-
-
-/**
-*print_number - Entry to code
-*
-* @n : integer to print
-*
-* Return: Always all number (Success)
-*/
 void print_number(int n)
 {
 	unsigned int num = n;
@@ -86,24 +10,10 @@ void print_number(int n)
 		_putchar('-');
 		num = -num;
 	}
-
-	else if (num <= 9)
+	if (num > 9)
 	{
-		_putchar(num + '0');
+		print_number(num / 10);
 	}
-
-	else if (num <= 99)
-	{
-		two_digit(num + '0');
-	}
-
-	else if (num <= 999)
-	{
-		three_digit(num + '0');
-	}
-
-	else if (num <= 9999)
-	{
-		four_digit(num + '0');
-	}
+	_putchar(num % 10 + '0');
 }
+
