219ac05256000f55a835f9465dc43433eadf3247
HMDT2023 <merveilhouenagnon@gmail.com>
2023-02-27 17:23:28 -0800

    review of betty style of programs 1 to 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,16 @@
 
 void rev_string(char *s)
 {
-	int i, j = 0;
+	int i, l, j = 0;
 	char *p;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		p[i] = s[i];
+	}
+
+	for (l = 0; *(str + l) != '\0'; l++)
+	{
 	}
 
 	for (i = (l - 1) ; i >= 0; i--)
