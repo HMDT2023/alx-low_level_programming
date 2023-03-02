75a557c80eb2cea82c8f29fcbf199fd02317c20a
HMDT2023 <merveilhouenagnon@gmail.com>
2023-03-01 18:45:22 -0800

    program 1 pointer

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,5 +13,16 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	return (strncat(dest, src, n));
+	int dest_len = strlen(dest);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+
+	dest[dest_len + i] = '\0';
+
+	return (dest);
 }
+
