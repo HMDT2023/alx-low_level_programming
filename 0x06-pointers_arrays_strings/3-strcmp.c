dbd274fca1f7ed9e4813cda361be6d55d0d73b64
HMDT2023 <merveilhouenagnon@gmail.com>
2023-03-02 05:37:34 -0800

    program 0 to 3 for pointer

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,9 +16,9 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0, j = 0; s1[i] != '\0' || s2[j] != '\0'; i++, j++)
 	{
 		if (s1[i] < s2[j])
-			return (-15);
+			return (-1);
 		if (s1[i] > s2[j])
-			return (15);
+			return (1);
 	}
 	return (0);
 }
