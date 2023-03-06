2c108b15f3a28e369948c616eb6bffb7c1f6f86f
HMDT2023 <merveilhouenagnon@gmail.com>
2023-03-06 11:21:00 -0800

    Program 0 and 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,13 +4,13 @@
 
 /**
   *_memset - Entry code
-  *@dest: 1st char, destination
-  *@src: 2nd char, source
+  *@s: 1st char
+  *@b: 2nd char
   *@n: bytes
   *Return: a character
   */
 
-char *_memset(char *dest, char *src, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	return (memset(s, b, n));
 }
