f74a8ee55d7f9d605c73f83e44af5cc6a53569e3
HMDT2023 <merveilhouenagnon@gmail.com>
2023-03-06 11:10:45 -0800

    Program 0 and 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+  *_memcpy - Entry code
+  *@dest: 1st char, destination
+  *@src: 2nd char, source
+  *@n: bytes
+  *Return: a character
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (memcpy(s, b, n));
+}
